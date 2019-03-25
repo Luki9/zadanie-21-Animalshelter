b21c6fd09d82401fb664d2d014f391767de9735d
lukasz <robson27-r@hotmail.com>
2019-03-25 16:37:28 +0100

    w trakcie 5

--- src/main/java/lc/animalshelter/demo/Controller/HomeController.java
+++ src/main/java/lc/animalshelter/demo/Controller/HomeController.java
@@ -1,4 +1,17 @@
 package lc.animalshelter.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "home";
+
+    }
 }
